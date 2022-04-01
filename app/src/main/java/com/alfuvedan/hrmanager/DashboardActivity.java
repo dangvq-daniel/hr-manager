ca2640529aecb378acce91f248015903c8ea25c5
dangvq-daniel <57842685+dangvq-daniel@users.noreply.github.com>
2022-04-01 01:51:59 -0400

    DashBoard Activity added

--- app/src/main/java/com/alfuvedan/hrmanager/DashboardActivity.java
+++ app/src/main/java/com/alfuvedan/hrmanager/DashboardActivity.java
@@ -1,6 +1,35 @@
 package com.alfuvedan.hrmanager;
 
-import android.app.Activity;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
 
-public class DashboardActivity extends Activity {
+import com.alfuvedan.hrmanager.data.*;
+
+public class DashboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+    }
+
+    public void goToEmployeeTable(){
+        Intent intent = new Intent(this, EmployeeTableActivity.class);
+        startActivity(intent);
+    }
+
+    public void gotoOption(){
+        if (1 == LoginInfo.getAcessTier()) {
+            Intent intent = new Intent(this, OptionLevel1.class);
+            startActivity(intent);
+        }
+        else if (LoginInfo.getAcessTier() == 2){
+            Intent intent = new Intent(this, OptionLevel2.class);
+            startActivity(intent);
+        }
+        else{
+            throw new IllegalArgumentException("Invalid Access Tier");
+        }
+    }
 }
